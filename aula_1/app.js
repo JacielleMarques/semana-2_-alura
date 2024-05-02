96b64b24553cc050c47699a671b53580d4787139
JacielleMarques <100531227+JacielleMarques@users.noreply.github.com>
2024-05-02 19:05:02 -0300

    Add files via upload

---
+++ aula_1/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1')
+titulo .innerHTML = 'Jogo número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolhar um número entre 1 e 10';
+
+function verificarChute() {
+    console.log('O botão foi clicado!')
+}
---
+++ aula_3/desafio_3/app.js
@@ -0,0 +1,57 @@
+
+function calculaIMC(altura, peso){
+
+    let imc = peso / (alturaMetros * alturaMetros);
+  }
+  
+  function calcularFatorial(numero) {
+    if (numero === 0 || numero === 1) {
+      return 1;
+    }
+  
+    let fatorial = 1;
+    for (let i = 2; i <= numero; i++) {
+      fatorial *= i;
+    }
+  
+    return fatorial;
+  }
+  
+  // Exemplo de uso
+  let numero = 5;
+  let resultado = calcularFatorial(numero);
+  console.log(`O fatorial de ${numero} é ${resultado}`);
+
+  function converterDolarParaReal(valorEmDolar) {
+    let cotacaoDolar = 4.80;
+    let valorEmReais = valorEmDolar * cotacaoDolar;
+    return valorEmReais.toFixed(2);
+  }
+  
+  // Exemplo de uso
+  let valorEmDolar = 50;
+  let valorEmReais = converterDolarParaReal(valorEmDolar);
+  console.log(`${valorEmDolar} dólares equivalem a R$ ${valorEmReais}`);
+
+  function converterDolarParaReal(valorEmDolar) {
+    let cotacaoDolar = 4.80;
+    let valorEmReais = valorEmDolar * cotacaoDolar;
+    return valorEmReais.toFixed(2);
+  }
+  
+  // Exemplo de uso
+  let valorEmDolar = 50;
+  let valorEmReais = converterDolarParaReal(valorEmDolar);
+  console.log(`${valorEmDolar} dólares equivalem a R$ ${valorEmReais}`);
+
+  function calcularAreaPerimetroSalaCircular(raio) {
+    let area = Math.PI * raio * raio;
+    let perimetro = 2 * Math.PI * raio;
+    
+    console.log(`Área da sala circular: ${area.toFixed(2)} metros quadrados`);
+    console.log(`Perímetro da sala circular: ${perimetro.toFixed(2)} metros`);
+  }
+  
+  // Exemplo de uso
+  let raio = 4; // em metros
+  calcularAreaPerimetroSalaCircular(raio);
